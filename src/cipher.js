5bc281f470c57c0a9623d0312d3416ef086bed8a
Malui Gomez <luisagomezrodela@gmail.com>
2023-08-02 00:03:08 -0600

    se agrego la funcion de descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,5 @@
-export function cipher(textoPorCifrar, offset) {
-  let cipherText = "";
+export function encode(textoPorCifrar, offset) {
+  let encodeText = "";
 
   // loop para asegurarnos que la función trabaja con cada uno de los caracteres que se encuentra
   for (let i = 0; i < textoPorCifrar.length; i++) {
@@ -13,15 +13,38 @@ export function cipher(textoPorCifrar, offset) {
     if (isAlphabetLetter) {
       const base =
         char.toUpperCase() === char ? "A".charCodeAt(0) : "a".charCodeAt(0);
-      console.log("paso1", char.toUpperCase());
       const charCode = char.charCodeAt(0);
       const shiftedCharCode = ((charCode - base + offset) % 26) + base;
-      cipherText += String.fromCharCode(shiftedCharCode);
+      encodeText += String.fromCharCode(shiftedCharCode);
     } else {
       // contraparte de la funcion if para indicar que si se encuentra cualquier otro caracter que no sea alfabético se quedará sin cambios
-      cipherText += char;
+      encodeText += char;
     }
   }
 
-  return cipherText;
+  return encodeText;
+}
+
+function paraNegativos(x, y) {
+  // Custom modulo function to handle negative numbers correctly
+  return ((x % y) + y) % y;
+}
+
+export function decode(textoPorDescifrar, offset2) {
+  let decodeText = "";
+  for (let i = 0; i < textoPorDescifrar.length; i++) {
+    const char = textoPorDescifrar[i];
+    const isAlphabetLetter = /^[A-Za-z]$/.test(char);
+    if (isAlphabetLetter) {
+      const base =
+        char.toUpperCase() === char ? "A".charCodeAt(0) : "a".charCodeAt(0);
+      const charCode = char.charCodeAt(0);
+      const shiftedCharCode =
+        paraNegativos(charCode - base - offset2, 26) + base;
+      decodeText += String.fromCharCode(shiftedCharCode);
+    } else {
+      decodeText += char;
+    }
+  }
+  return decodeText;
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import { cipher } from "./cipher.js";
+import { encode } from "./cipher.js";
+import { decode } from "./cipher.js";
 
 document.getElementById("inicio").style.display = "block";
 document.getElementById("empezarCifrar").style.display = "none";
 document.getElementById("textoCifrado").style.display = "none";
+document.getElementById("empezarDescifrar").style.display = "none";
+document.getElementById("textoDescifrado").style.display = "none";
 
-// division para poder ver todas las funciones que le estoy intentando anadir a un solo boton
+// division para poder ver todas las funciones que le estoy intentando anadir a un solo botón
 function resetearCajaTexto() {
   document.getElementById("cajaTexto").value = "";
 }
@@ -15,10 +18,8 @@ function resetearOffset() {
 
 function empezarCifrarF() {
   resetearCajaTexto();
-  const section = document.getElementById("inicio");
-  section.style.display = "none";
-  const section2 = document.getElementById("empezarCifrar");
-  section2.style.display = "block";
+  document.getElementById("inicio").style.display = "none";
+  document.getElementById("empezarCifrar").style.display = "block";
 }
 
 function empezarCifrarConReset() {
@@ -35,10 +36,10 @@ function cifrarF() {
   const textoPorCifrar = document.getElementById("cajaTexto").value;
   const offset = parseInt(document.getElementById("lugaresOffset").value);
 
-  // Perform encryption (using the cipher function from cipher.js)
-  const textoCifradoC = cipher(textoPorCifrar, offset);
+  // Realizar el cifrado (utilizando la función cipher de cipher.js)
+  const textoCifradoC = encode(textoPorCifrar, offset);
 
-  // Display the encrypted text
+  // Mostrar el texto cifrado
   const parrafoCifrado = document
     .getElementById("textoCifrado")
     .querySelector("p");
@@ -52,10 +53,62 @@ function cifrarF() {
 document.getElementById("cifrarB").addEventListener("click", cifrarF);
 
 function inicioF() {
-  const section3 = document.getElementById("textoCifrado");
-  section3.style.display = "none";
-  const section = document.getElementById("inicio");
-  section.style.display = "block";
+  document.getElementById("textoCifrado").style.display = "none";
+  document.getElementById("textoDescifrado").style.display = "none";
+  document.getElementById("inicio").style.display = "block";
 }
 
 document.getElementById("inicioB").addEventListener("click", inicioF);
+
+// función combinada para resetear texto cada vez que se quiera descifrar un nuevo texto
+function resetearCajaTexto2() {
+  document.getElementById("cajaTexto2").value = "";
+}
+
+function resetearOffset2() {
+  document.getElementById("lugaresOffset2").value = "";
+}
+
+function empezarDescifrarF() {
+  resetearCajaTexto2();
+  document.getElementById("textoCifrado").style.display = "none";
+  document.getElementById("empezarDescifrar").style.display = "block";
+}
+
+function empezarDescifrarConReset() {
+  empezarDescifrarF();
+  resetearOffset2();
+}
+
+document
+  .getElementById("empezarDescifrarB")
+  .addEventListener("click", empezarDescifrarConReset);
+
+document
+  .getElementById("empezarDescifrarB")
+  .addEventListener("click", empezarDescifrarF);
+
+// aquí empieza la función de descifrado
+
+function descifrarF() {
+  const textoPorDescifrar = document.getElementById("cajaTexto2").value;
+  const offset2 = parseInt(document.getElementById("lugaresOffset2").value);
+
+  // Realizar el descifrado (utilizando la función cipher de cipher.js)
+  const textoDescifradoC = decode(textoPorDescifrar, offset2);
+
+  // Mostrar el texto cifrado
+  const parrafoDescifrado = document
+    .getElementById("textoDescifrado")
+    .querySelector("p");
+  parrafoDescifrado.textContent = textoDescifradoC;
+
+  // Para ver la sección de "textoCifrado"
+  document.getElementById("empezarDescifrar").style.display = "none";
+  document.getElementById("textoDescifrado").style.display = "block";
+}
+
+document.getElementById("descifrarB").addEventListener("click", descifrarF);
+
+// reutilizando formula de inicio, únicamente añadiéndole el display de esta sección a la función original
+document.getElementById("inicioB2").addEventListener("click", inicioF);
